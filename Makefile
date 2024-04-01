dd0f1fc3eb8be79cc5c892a846faf64b8891e0b2
최준형 <k14414@naver.com>
2024-04-01 20:57:07 +0900

    ing

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 main.exe: main.o
-	g++ -o main.exe main.o console.o
+	g++ -o main.exe main.o database.o
 
 main.o: main.cpp
 	g++ -Werror -std=c++11 -c main.cpp
 
-console.o: console.cpp
-	g++ -Werror -std=c++11 -c console.cpp
+console.o: database.cpp
+	g++ -Werror -std=c++11 -c database.cpp
---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+#include "database.h"
+
+int main()
+{
+    // while(true)
+    // {
+        
+    //     std::string command;
+
+    //     std::cout << "command (list, add, get, edl, exit): ";
+    //     std::cin >> command;
+    // }
+    struct Database a;
+
+
+    
+
+
+    
+   
+}
+
